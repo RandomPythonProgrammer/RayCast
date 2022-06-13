0b910c4c28dc675a816791ccd05a85b0551a675b
RandomPythonProgrammer <74526339+RandomPythonProgrammer@users.noreply.github.com>
2022-06-12 20:28:53 -0500

    Major Changes

--- main.py
+++ main.py
@@ -5,26 +5,40 @@ import numpy
 import pyglet
 from PIL import Image
 from numba import njit
+from numba.pycc import CC
+cc = CC("game")
+
+VIEW_RANGE = 250
+BRIGHTNESS = 175
+SHOW_FPS = False
 
 
-@njit
+@njit(cache=True)
+@cc.export('get_lines', 'int64[:, :](int64, float64, int64, float64, float64, int64, int64, uint8[:, :, :])')
 def get_lines(real_width: int, view_angle: float, height: int, scale: float, rotation: float, x: int, y: int, map_data: numpy.ndarray):
-    columns = range(-round(real_width / 2), round(real_width / 2))
-    return_array = numpy.ndarray(shape=(len(columns), 6), dtype="int64")
-    index = 0
+    half_width = real_width/2
+    columns = range(-round(half_width), round(half_width))
+    return_array = numpy.zeros(shape=(len(columns), 6), dtype="int64")
+    rotation_offset = view_angle / real_width
     for column in columns:
-        rotation_offset = view_angle / real_width
         angle = math.radians(rotation_offset * column - rotation)
-        r, g, b, a, distance, px, py = cast_ray(angle, 50, map_data, x, y)
+        result = cast_ray(angle, VIEW_RANGE, map_data, x, y)
+        color = result[:3]
+        distance, px, py = result[4:]
+        distance = distance/numpy.power(VIEW_RANGE, 0.75)
+        if distance > 0:
+            shade = 1 / numpy.power(max(distance, 1), 1.5)
+            color = (color * shade).clip(0, BRIGHTNESS)
+        r, g, b = color
         column_x = round((column + real_width / 2) * scale)
-        column_height = round(height / max(distance, 1))
+        column_height = round(height / distance)
         height_offset = round((height - column_height) / 2)
-        return_array[index] = numpy.array([column_x, height_offset, column_height, r, g, b], dtype="int64")
-        index += 1
+        return_array[round(column + half_width)] = numpy.array([column_x, height_offset, column_height, r, g, b], dtype="int64")
     return return_array
 
 
-@njit
+@njit(cache=True)
+@cc.export('cast_ray', 'float64[:](float64, int64, uint8[:, :, :], int64, int64)')
 def cast_ray(angle: float, limit: int, map_data: numpy.ndarray, x: int, y: int):
     width, height, depth = map_data.shape
     dx, dy = numpy.cos(angle), numpy.sin(angle)
@@ -34,7 +48,7 @@ def cast_ray(angle: float, limit: int, map_data: numpy.ndarray, x: int, y: int):
         if 0 <= py < height and 0 <= px < width:
             temp_color = map_data[py][px]
             if temp_color[-1] > 0:
-                distance = ((px - x) ** 2 + (py - (height - 1 - y)) ** 2) ** 0.5
+                distance = numpy.sqrt(numpy.square((px - x)) + numpy.square((py - (height - 1 - y))))
                 r, g, b, a = temp_color
                 return numpy.array([r, g, b, a, distance, px, py], dtype="float64")
         else:
@@ -45,7 +59,8 @@ def cast_ray(angle: float, limit: int, map_data: numpy.ndarray, x: int, y: int):
 class Game(pyglet.window.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
-        self.scale = 0.1
+        self.set_vsync(False)
+        self.scale = 8
         self.x = self.y = self.rotation = 0
         self.real_width = round(self.width / self.scale)
         self.view_angle = 90
@@ -64,9 +79,8 @@ class Game(pyglet.window.Window):
     def cast_rays(self):
         self.lines.clear()
         lines = get_lines(self.real_width, self.view_angle, self.height, self.scale, self.rotation, self.x, self.y, self.map_data)
-
-        for line in lines:
-            x, height_offset, height, r, g, b = line
+        for i in range(lines.shape[0]):
+            x, height_offset, height, r, g, b = lines[i]
             self.lines.append(pyglet.shapes.Line(
                 x, height_offset,
                 x, height + height_offset,
@@ -92,13 +106,13 @@ class Game(pyglet.window.Window):
 
         for column in range(-round(self.real_width / 2), round(self.real_width / 2)):
             angle = math.radians(rotation_offset * column - self.rotation)
-            r, g, b, a, distance, px, py = cast_ray(angle, 50, self.map_data, self.x, self.y)
+            r, g, b, a, distance, px, py = cast_ray(angle, VIEW_RANGE, self.map_data, self.x, self.y)
             if distance >= 0:
                 draw.line(((self.x, image.height - 1 - self.y), (px, py)))
         image.show()
 
-    def update(self, dt):
-        speed = 15 * dt
+    def update(self, dt: float):
+        speed = 75 * dt
         rotation_speed = 50 * dt
         rotation = self.rotation
         radians = math.radians(rotation)
@@ -118,9 +132,16 @@ class Game(pyglet.window.Window):
             self.y += dy
 
         self.rotation = rotation % 360
+        try:
+            print(1/dt)
+        except ZeroDivisionError:
+            pass
 
 
 if __name__ == '__main__':
+    print("compiling...")
+    cc.compile()
+    print("done.")
     game = Game(1024, 640)
     pyglet.clock.schedule(game.render)
     pyglet.clock.schedule(game.update)
